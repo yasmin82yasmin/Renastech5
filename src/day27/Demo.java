01da774c13fb7cdf7b04d7efb239ea02c614f813
yasmin82yasmin <yasmin_82_yasmin@outlook.com>
2022-01-24 22:25:21 -0500

    interface

--- src/day27/Demo.java
+++ src/day27/Demo.java
@@ -1,32 +1,29 @@
 package day27;
 
 public class Demo {
-    public void main(String[] args) {
-
-
+    public static void main(String[] args) {
         B b = new B();
-        b.setA(56);
+        b.setA((55));
         System.out.println(b.getA());
         b.show();
     }
-
-    class A {
-        int a;
-
-        void show() {System.out.println("Hello");
-        }
-
-        int getA() {return this.a;}
-
-        void setA(int a) { this.a = a; }
-
-    }
-
-    class B extends A {
-        @Override
-        void show() {
-            System.out.println("Hi");
-        }
-    }
-
 }
+class A{
+    int a;
+    void show(){
+        System.out.println("Hello");
+    }
+    int getA(){return this.a;}
+    void setA(int a){this.a =a;}
+}
+
+class B extends A{
+    void show(){
+        System.out.println("Hi");
+    }
+}
+
+
+
+
+
